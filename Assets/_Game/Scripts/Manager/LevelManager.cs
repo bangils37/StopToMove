011ae0261a1b93347efb6bf42f91e5016fdfd41e
bangils37 <nguyenbanganh30@gmail.com>
2023-07-07 18:14:50 +0700

    Push Code

--- Assets/_Game/Scripts/Manager/LevelManager.cs
+++ Assets/_Game/Scripts/Manager/LevelManager.cs
@@ -1,197 +1,208 @@
-﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.UIElements;
 
-public class LevelManager : Singleton<LevelManager>         /// Fix lại cái mà đứng trong vòng target của nhau nhưng không ném vũ khí
+public class LevelManager : Singleton<LevelManager>
 {
-    [SerializeField] private GameObject enemys;
-    [SerializeField] private ColorScriptableObject colorScriptableObject;
+    public Player player;
 
-    [SerializeField] private PlayerController player;
+    private List<Bot> bots = new List<Bot>();
 
-    [SerializeField] private int numberEnemyInMap;
-    [SerializeField] private int allEnemy;
-    [SerializeField] private float timer;
-    [SerializeField] private int numberAliveEnemy;
-    private GameObject[] enemysGameObject;
-    private int countEnemys;
-    private int centroidSpawnPosition;
-    private int[,] matrixMap;
-    private float rectMap;
+    [SerializeField] Level[] levels;
+    public Level currentLevel;
 
-    private void Start()
+    private int totalBot;
+    private bool isRevive;
+
+    private int levelIndex;
+
+    public int TotalCharater => totalBot + bots.Count + 1;
+
+    public void Start()
     {
-        timer = 0;
-        numberAliveEnemy = 0;
-        matrixMap = new int[5, 5];
-        centroidSpawnPosition = 5;
-        rectMap = 200;
-
-        countEnemys = enemys.transform.childCount;
-        enemysGameObject = new GameObject[countEnemys];
-
-        for(int i=0;i<countEnemys;i++)
-        {
-            enemysGameObject[i] = enemys.transform.GetChild(i).gameObject;
-        }
-        //Debug.Log(countEnemys);
+        levelIndex = 0;
+        OnLoadLevel(levelIndex);
+        OnInit();
     }
 
-    private void Update()
+    public void OnInit()
     {
-        timer -= Time.deltaTime;
-        if (timer < 0f)
-        {
-            timer = 4;
+        player.OnInit();
 
-            UpdateMatrixMap();
-            SpawnFullEnemyInMap();
+        for (int i = 0; i < currentLevel.botReal; i++)
+        {
+            NewBot(null);
         }
+
+        totalBot = currentLevel.botTotal - currentLevel.botReal - 1;
+
+        isRevive = false;
+
+        SetTargetIndicatorAlpha(0);
     }
 
-    //private void InitMatrixMap()
-    //{
-    //    building = new Transform[mapBuildings.transform.childCount];
-
-    //    for (int i = 0; i < mapBuildings.transform.childCount; i++)
-    //    {
-    //        building[i] = mapBuildings.transform.GetChild(i);
-    //    }
-
-    //    matrixMap = new int[5, 5];
-    //    /// maxtrixMap[i, j] = 0 <=> Không có enemy nào ở trong khu vực này
-    //    /// maxtrixMap[i, j] = k <=> Có k enemy ở trong khu vực này
-
-    //    for (int i = 0; i < 5; i++)
-    //    {
-    //        for (int j = 0; j < 5; j++)
-    //        {
-    //            matrixMap[i, j] = 0;
-    //        }
-    //    }
-    //}
-
-    internal void UpdateMatrixMap()
+    public void OnReset()
     {
-        int x = new int();
-        int z = new int();
-
-        for (int i = 0; i < 5; i++) for (int j = 0; j < 5; j++)
-            matrixMap[i, j] = 0;
-
-        GetMatrixMapFromPosition(player.transform.position, ref x, ref z);
-        matrixMap[x, z]++;
-
-        for (int i = 0; i < countEnemys; i++)
+        player.OnDespawn();
+        for (int i = 0; i < bots.Count; i++)
         {
-            if (enemysGameObject[i].active)
+            bots[i].OnDespawn();
+        }
+
+        bots.Clear();
+        SimplePool.CollectAll();
+    }
+
+    public void OnLoadLevel(int level)
+    {
+        if (currentLevel != null)
+        {
+            Destroy(currentLevel.gameObject);
+        }
+
+        currentLevel = Instantiate(levels[level]);
+    }
+
+    public Vector3 RandomPoint()
+    {
+        Vector3 randPoint = Vector3.zero;
+
+        float size = Character.ATT_RANGE + Character.MAX_SIZE + 1f;
+
+        for (int t = 0; t < 50; t++)
+        {
+
+            randPoint = currentLevel.RandomPoint();
+            if (Vector3.Distance(randPoint, player.TF.position) < size)
             {
-                GetMatrixMapFromPosition(enemysGameObject[i].transform.position, ref x, ref z); 
-                matrixMap[x, z]++;
-                //Debug.Log("haha " + x + " hihi " + z);
-            }    
-            //Debug.Log(enemysTransform[i].position);
-        }
-        //Debug.Log("=========================");
-    }
+                continue;
+            }
 
-    internal void GetAreaFromMatrixMap(ref float x1, ref float x2, ref float z1, ref float z2, int x, int z)
-    {
-        x1 = x * (rectMap/5) - (rectMap/2);
-        x2 = (x + 1) * (rectMap / 5) - (rectMap / 2);
-        z1 = z * (rectMap / 5) - (rectMap / 2);
-        z2 = (z + 1) * (rectMap / 5) - (rectMap / 2);
-    }
-
-    internal void GetMatrixMapFromPosition(Vector3 pos, ref int x, ref int z)
-    {
-        x = (int)((pos.x + (rectMap / 2)) / (rectMap / 5));
-        z = (int)((pos.z + (rectMap / 2)) / (rectMap / 5));
-    }
-
-    internal Vector3 GetPlayerPosition()
-    {
-        return player.transform.position;
-    }
-
-    private void OnApplicationQuit()
-    {
-
-    }
-
-    #region Enemy
-    List<EnemyController> listEnemy = new List<EnemyController>();
-
-    public void SpawnFullEnemyInMap()
-    {
-        if (allEnemy >= numberEnemyInMap)
-        {
-            int tmp = numberEnemyInMap - numberAliveEnemy;
-            for (int i = 0; i < tmp; i++)
+            for (int j = 0; j < 20; j++)
             {
-                SpawnEnemy();
+                for (int i = 0; i < bots.Count; i++)
+                {
+                    if (Vector3.Distance(randPoint, bots[i].TF.position) < size)
+                    {
+                        break;
+                    }
+                }
+
+                if (j == 19)
+                {
+                    return randPoint;
+                }
+            }
+
+
+        }
+
+        return randPoint;
+    }
+
+    private void NewBot(IState<Bot> state)
+    {
+        Bot bot = SimplePool.Spawn<Bot>(PoolType.Bot, RandomPoint(), Quaternion.identity);
+        bot.OnInit();
+        bot.ChangeState(state);
+        bots.Add(bot);
+
+        bot.SetScore(player.Score > 0 ? Random.Range(player.Score - 7, player.Score + 7) : 1);
+    }
+
+    public void CharecterDeath(Character c)
+    {
+        if (c is Player)
+        {
+            UIManager.Ins.CloseAll();
+
+            //revive
+            if (!isRevive)
+            {
+                isRevive = true;
+                UIManager.Ins.OpenUI<UIRevive>();
+            }
+            else
+            {
+                Fail();
             }
         }
-    }
-
-    public void SpawnEnemy()
-    {
-        Vector3 pos = new Vector3();
-        float x1 = new float();
-        float x2 = new float();
-        float z1 = new float();
-        float z2 = new float();
-
-        for(int i=0;i<5;i++)
+        else
+        if (c is Bot)
         {
-            for(int j=0;j<5;j++)
+            bots.Remove(c as Bot);
+
+            if (GameManager.Ins.IsState(GameState.Revive) || GameManager.Ins.IsState(GameState.Setting))
             {
-                if (matrixMap[i,j] == 0)
+                NewBot(Utilities.Chance(50, 100) ? new IdleState() : new PatrolState());
+            }
+            else
+            {
+                if (totalBot > 0)
                 {
-                    matrixMap[i,j]++;
-                    GetAreaFromMatrixMap(ref x1, ref x2, ref z1, ref z2, i, j);
-                    pos = RandomPosition(x1 + centroidSpawnPosition, x2 - centroidSpawnPosition, z1 + centroidSpawnPosition, z2 - centroidSpawnPosition);
-
-                    EnemyController enemy = SimplePool.Spawn<EnemyController>(PoolType.Enemy, pos, Quaternion.identity);
-                    enemy.OnInit();
-                    enemy.AddLevel(UnityEngine.Random.Range(player.characterLevel > 2? player.characterLevel - 2 : player.characterLevel, player.characterLevel + 3));
-
-                    /////Debug
-                    //int x = new int();
-                    //int z = new int();
-                    //Debug.Log(pos);
-                    //GetMatrixMapFromPosition(pos, ref x, ref z);
-                    //Debug.Log("x la " + x + " z la " + z);
-                    //Debug.Log("haha " + i + " hihi " + j);
-                    /////End
-                    return;
+                    totalBot--;
+                    NewBot(Utilities.Chance(50, 100) ? new IdleState() : new PatrolState());
                 }
-            }    
-        }    
+
+                if (bots.Count == 0)
+                {
+                    Victory();
+                }
+            }
+
+        }
+
+        UIManager.Ins.GetUI<UIGameplay>().UpdateTotalCharacter();
     }
 
-    private Vector3 RandomPosition(float x1, float x2, float z1, float z2)
+    private void Victory()
     {
-        Vector3 pos = new Vector3(UnityEngine.Random.Range(x1, x2), 0, UnityEngine.Random.Range(z1, z2));
-
-        return pos;
+        UIManager.Ins.CloseAll();
+        UIManager.Ins.OpenUI<UIVictory>().SetCoin(player.Coin);
+        player.ChangeAnim(Constant.ANIM_WIN);
     }
 
-    public void EnemyInit(EnemyController enemy)
+    public void Fail()
     {
-        numberAliveEnemy++;
-        listEnemy.Add(enemy);
+        UIManager.Ins.CloseAll();
+        UIManager.Ins.OpenUI<UIFail>().SetCoin(player.Coin); 
     }
 
-    public void EnemyDespawn(EnemyController enemy)
+    public void Home()
     {
-        numberAliveEnemy--;
-        allEnemy--;
-        listEnemy.Remove(enemy);
+        UIManager.Ins.CloseAll();
+        OnReset();
+        OnLoadLevel(levelIndex);
+        OnInit();
+        UIManager.Ins.OpenUI<UIMainMenu>();
     }
 
-    #endregion
+    public void NextLevel()
+    {
+        levelIndex++;
+    }
+
+    public void OnPlay()
+    {
+        for (int i = 0; i < bots.Count; i++)
+        {
+            bots[i].ChangeState(new PatrolState());
+        }
+    }
+
+    public void OnRevive()
+    {
+        player.TF.position = RandomPoint();
+        player.OnRevive();
+    }
+
+    public void SetTargetIndicatorAlpha(float alpha)
+    {
+        List<GameUnit> list = SimplePool.GetAllUnitIsActive(PoolType.TargetIndicator);
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            (list[i] as TargetIndicator).SetAlpha(alpha);
+        }
+    }
 }
